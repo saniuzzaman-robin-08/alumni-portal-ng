c5d0bf02efd2e8c4f430ef11276391099cc2e65d
Saniuzzaman Robin <32646654+saniuzzaman-robin-08@users.noreply.github.com>
2022-12-05 00:49:59 +0600

    integrate error-404 page

--- src/app/auth/components/forgot-password/forgot-password.component.ts
+++ src/app/auth/components/forgot-password/forgot-password.component.ts
@@ -1,15 +1,44 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
+import { AlumniSnackbarService } from '../../../shared/services/alumni-snackbar.service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit {
-
-  constructor() { }
+  resetPasswordForm: FormGroup;
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _store: Store,
+    private _router: Router,
+    private _alumniSnackbarService: AlumniSnackbarService
+  ) {}
 
   ngOnInit(): void {
+    this.resetPasswordForm = this._formBuilder.group({
+      Email: ['', [Validators.required]],
+    });
   }
-
+  resetPassword() {
+    if (this.resetPasswordForm.valid && this.resetPasswordForm.get('Email').value) {
+      this._alumniSnackbarService.openSnackbar(
+        'success',
+        `If an account with that email exist, you'll recieve a reset password link!`
+      );
+      this._router.navigate(['login']);
+    } else {
+      this.resetPasswordForm.markAllAsTouched();
+      this._alumniSnackbarService.openSnackbar(
+        'error',
+        `Please enter a valid mail`
+      );
+    }
+  }
+  login() {
+    this._router.navigate(['login']);
+  }
 }
